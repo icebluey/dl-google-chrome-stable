b712a79d6c8073c9adb921c8649f639087ec79cd
Kenneth Davidson <xylene2016@protonmail.com>
2026-02-13 21:34:34 -0500

    Update dl-google-chrome-stable.sh

--- dl-google-chrome-stable.sh
+++ dl-google-chrome-stable.sh
@@ -17,20 +17,23 @@ _install_7z() {
     set -euo pipefail
     local _tmp_dir="$(mktemp -d)"
     cd "${_tmp_dir}"
-    _7zip_loc=$(wget -qO- 'https://www.7-zip.org/download.html' | grep -i '\-linux-x64.tar' | grep -i 'href="' | sed 's|"|\n|g' | grep -i '\-linux-x64.tar' | sort -V | tail -n 1)
-    _7zip_ver=$(echo ${_7zip_loc} | sed -e 's|.*7z||g' -e 's|-linux.*||g')
-    wget -c -t 9 -T 9 "https://www.7-zip.org/${_7zip_loc}"
-    sleep 1
-    tar -xof *.tar.*
-    sleep 1
-    rm -f *.tar*
-    rm -f /usr/bin/7z
-    rm -f /usr/local/bin/7z
-    install -v -c -m 0755 7zzs /usr/bin/7z
-    cp -af /usr/bin/7z /usr/local/bin/7z
+    #_7zip_loc="$(wget -qO- 'https://www.7-zip.org/download.html' | grep -i '\-linux-x64.tar' | grep -i 'href="' | sed 's|"|\n|g' | grep -i '\-linux-x64.tar' | sort -V | tail -n 1)"
+    #wget -q -c -t 9 -T 9 "https://www.7-zip.org/${_7zip_loc}"
+    #tar -xof *.tar*
+    #sleep 1
+    #rm -f *.tar*
+    #file 7zzs | sed -n -E 's/^(.*):[[:space:]]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' strip '{}'
+    #rm -f 7z && mv 7zzs 7z
+    wget -q -c -t 9 -T 9 'https://github.com/icebluey/7zip-zstd/releases/latest/download/7z.tar'
+    wget -q -c -t 9 -T 9 'https://github.com/icebluey/7zip-zstd/releases/latest/download/7z.tar.sha256'
+    sha256sum -c 7z.tar.sha256
+    tar -xof 7z.tar
+    rm -f /usr/bin/7z /usr/local/bin/7z
+    install -v -c -m 0755 7z /usr/bin/7z
+    cp -f /usr/bin/7z /usr/local/bin/7z
+    /usr/bin/7z --version 2>/dev/null || true
     cd /tmp
     rm -fr "${_tmp_dir}"
-    /sbin/ldconfig
 }
 _install_7z
 
